2183083848be38ec0843587ffb17b93171340af9
eladsabag <68230346+eladsabag@users.noreply.github.com>
2021-12-26 21:51:59 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,45 @@
-document.getElementsByClassName("start")[0].addEventListener("click", () => {changeOpacity("content",0)});
-document.getElementsByClassName("start")[1].addEventListener("click", () => {changeOpacity("content",1)});
-document.getElementsByClassName("start")[2].addEventListener("click", () => {changeOpacity("content",2)});
-document.getElementsByClassName("start")[3].addEventListener("click", () => {changeOpacity("content",3)});
-document.getElementsByClassName("start")[4].addEventListener("click", () => {changeOpacity("finish",0)});
+// Start,Next,Finish Buttons Opcaity
+
+document.querySelectorAll(".start")[0].addEventListener("click", () => {changeOpacity(".content",0)});
+document.querySelectorAll(".start")[1].addEventListener("click", () => {changeOpacity(".content",1)});
+document.querySelectorAll(".start")[2].addEventListener("click", () => {changeOpacity(".content",2)});
+document.querySelectorAll(".start")[3].addEventListener("click", () => {changeOpacity(".content",3)});
+document.querySelectorAll(".start")[4].addEventListener("click", () => {changeOpacity(".finish",0)});
+
 var flow = 0;
 
 function changeOpacity(name,index) {
   if(index<=flow) {
     flow+=1;
-    document.getElementsByClassName(name)[index].style.opacity = 1;
+    document.querySelectorAll(name)[index].style.opacity = 1;
   } else {
     alert("Can't break flow!");
+  }
+}
+
+// Start,Next,Finish Buttons Sounds
+
+for (var i = 0; i < document.querySelectorAll(".start").length ; i++) {
+  document.querySelectorAll(".start")[i].addEventListener("click", function() {
+      makeSound(this.innerHTML);
+  });
+}
+
+function makeSound(key) {
+  switch (key) {
+    case "Start":
+    var start = new Audio("sounds/jeff.mp3");
+    start.play();
+      break;
+    case "Next":
+    var next = new Audio("sounds/jeff.mp3");
+    next.play();
+      break;
+    case "Finish":
+    var finish = new Audio("sounds/johncena.mp3");
+    finish.play();
+      break;
+    default:
+      console.log("Error");
   }
 }
